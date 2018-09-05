c9322c032b4abcae8313502fd06ea280acb77687
Horace Lee <horacehylee2@gmail.com>
2018-09-06 00:20:22 +0800

    project init

---
+++ src/main/java/com/horacehylee/cucumbertryout/cucumbertryout/CucumberTryoutApplication.java
@@ -0,0 +1,12 @@
+package com.horacehylee.cucumbertryout.cucumbertryout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberTryoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumberTryoutApplication.class, args);
+	}
+}
---
+++ src/test/java/com/horacehylee/cucumbertryout/cucumbertryout/CucumberTryoutApplicationTests.java
@@ -0,0 +1,16 @@
+package com.horacehylee.cucumbertryout.cucumbertryout;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CucumberTryoutApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
